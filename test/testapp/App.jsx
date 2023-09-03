88ad7f0191123110b0c9c34b49e0c1f31f146153
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-04 00:57:37 +0500

    jsx and conditional operator

--- test/testapp/App.jsx
+++ test/testapp/App.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { View, Text, Button } from 'react-native'
 
 export default function App() {
+  let name = 'Ahmed'
+  let age = 19
   return (
+    <>
     <View style={{padding:50, backgroundColor:'blue'}}>
       <Text style={{fontSize:35 , fontWeight:900}}>Mobile App Developnment</Text>
       <Button title='Press ME' color={"red"}/>
     </View>
+    <View style={{backgroundColor:'lightpink', padding:50}}>
+      <Text>{name}</Text>
+      <Text>{age >= 19 ? `Your age is ${age}`  : ''}</Text>
+    </View>
+    </>
   )
 }
