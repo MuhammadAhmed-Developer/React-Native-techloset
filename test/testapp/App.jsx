bfcb3a2cc1aff6682267b8bee3974330579638e7
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-04 01:14:45 +0500

    Add component in app component

--- test/testapp/App.jsx
+++ test/testapp/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
 
-export default function App() {
+const  App=()=> {
   let name = 'Ahmed'
   let age = 19
   return (
@@ -10,10 +10,29 @@ export default function App() {
       <Text style={{fontSize:35 , fontWeight:900}}>Mobile App Developnment</Text>
       <Button title='Press ME' color={"red"}/>
     </View>
-    <View style={{backgroundColor:'lightpink', padding:50}}>
+    <View style={{backgroundColor:'black', padding:50}}>
       <Text>{name}</Text>
       <Text>{age >= 19 ? `Your age is ${age}`  : ''}</Text>
+      {
+        age==19?<Text>Your age is 19</Text>:<Text>Your age in not  19</Text>
+      }
+    </View>
+    <View style={{padding:50, backgroundColor:'green'}}>
+   <UserData/>
     </View>
     </>
   )
 }
+
+
+const UserData = () =>{
+  return(
+    <>
+    <Text>USer Name = Ahmed</Text>
+    <Text>USer Email = ahmed@gmail.com</Text>
+    </>
+  )
+}
+
+
+export  default App
